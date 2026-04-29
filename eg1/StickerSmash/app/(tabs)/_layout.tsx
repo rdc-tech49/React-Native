86363dcc7f4835d8b2fb3a81fcf935292f353b69
rdc-tech49 <rdc491994@gmail.com>
2026-04-29 07:41:54 +0530

    basic sticker smash

---
+++ eg1/StickerSmash/app/(tabs)/_layout.tsx
@@ -0,0 +1,51 @@
+import { Tabs } from "expo-router";
+
+import Ionicons from "@expo/vector-icons/Ionicons";
+
+export default function TabLayout() {
+  return (
+    <Tabs
+      screenOptions={{
+        tabBarActiveTintColor: "#3dff3dff",
+        headerStyle: {
+          backgroundColor: "#141517ff",
+        },
+        headerShadowVisible: false,
+        headerTintColor: "#f1e9e9ff",
+        tabBarStyle: {
+          backgroundColor: "#25292e",
+        },
+      }}
+    >
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: "Home",
+          headerTitle: "Sticker Smash",
+          tabBarIcon: ({ color, focused }) => (
+            <Ionicons
+              name={focused ? "home" : "home-outline"}
+              color={color}
+              size={24}
+            />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="about"
+        options={{
+          title: "About",
+          tabBarIcon: ({ color, focused }) => (
+            <Ionicons
+              name={
+                focused ? "information-circle" : "information-circle-outline"
+              }
+              color={color}
+              size={24}
+            />
+          ),
+        }}
+      />
+    </Tabs>
+  );
+}
---
+++ eg1/StickerSmash/app/(tabs)/index.tsx
@@ -0,0 +1,30 @@
+import { Text, View,  StyleSheet } from 'react-native';
+import { Link } from 'expo-router';
+
+export default function Index() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Home screen</Text>
+      <Link href="/about" style={styles.button}>
+        Go to About page
+      </Link>
+
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#25292e',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  text: {
+    color: '#fff',
+  },
+  button: {
+    color: '#b44d4dff',
+    marginTop: 20,
+  },
+});
